af9c2a7a1666489c8dc92770c5e78d8acea02f4e
Bayramzade066 <bayramzade.313@gmail.com>
2021-12-17 17:03:44 +0400

    Portfolio

--- Javascript/OOP/app.js
+++ Javascript/OOP/app.js
@@ -9,16 +9,108 @@
 // console.log(User('Rovshen',25));
 // console.log(User('Rustem',23));
 
-class User{
-    constructor(name,age){
-        this.name=name;
-        this.age=age;
+// class User{
+//     constructor(name,age){
+//         this.name=name;
+//         this.age=age;
+//     };
+//     sayHello(){
+//         console.log(`'Hello my name is ${this.name} and i am ${this.age} years old'`)
+//     }
+// }
+
+// let Mehemmed= new User('Mehemmed',20)
+
+// Mehemmed.sayHello()
+
+
+
+// class phone{
+//     constructor(model,color,inch,version){
+//         this.model=model;
+//         this.color=color;
+//         this.inch=inch;
+//         this.version=version;
+//     }
+
+//     getModel(){
+//         return this.model;
+//     }
+//     getColor(){
+//         return this.color;
+//     }
+//     getInch(){
+//         return this.inch;
+//     }
+//     getVersion(){
+//         return this.version;
+//     }
+// }
+
+
+// let myPhone = new phone('Iphone','Black','6.5-inch','x');
+
+// document.querySelector('body').innerHTML= 
+// myPhone.getModel()+" "+
+// myPhone.getVersion()+" "+
+// myPhone.getColor()+" "+
+// myPhone.getInch()+" ";
+
+class car{
+    constructor(model,color,id){
+        this.model=model;
+        this.color=color;
+        this.id=id;
     };
-    sayHello(){
-        console.log(`'Hello my name is ${this.name} and i am ${this.age} years old'`)
+
+    get Model(){
+        return this.model;
+    };
+    get Color(){
+        return this.color;
     }
-}
+    get Id(){
+        return this.id;
+    }
+    Message(){
+        return "Bu OOP test alertidir.";
+    }
+    set Model(model){
+        this.model=model;
+    };
+    set Color(color){
+        this.color=color;
+    };
+    set Id(id){
+        this.id=id;
+    };
+};
 
-let Mehemmed= new User('Mehemmed',20)
+class myCar extends car{
+    constructor(model,color,id,topspeed){
+        super(model,color,id);
+        this.topspeed=topspeed;
+    };
+    messageAlert(){
+        alert(super.Message());
+    }
+};
 
-Mehemmed.sayHello()
+let Car = new myCar('Mercedes-GLS230','black','3','360 km/h');
+
+document.querySelector('body').innerHTML=
+Car.Model+"<br>"+
+Car.Color+"<br>"+
+Car.Id+"<br>";
+
+Car.model='Tesla model x';
+Car.color='Gray';
+Car.id='2';
+
+document.querySelector('body').innerHTML+=
+Car.Model+"<br>"+
+Car.Color+"<br>"+
+Car.Id+"<br>"+
+Car.topspeed+"<br>";
+Car.messageAlert();
+  
