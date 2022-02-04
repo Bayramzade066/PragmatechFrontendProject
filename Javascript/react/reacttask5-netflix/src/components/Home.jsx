bf535c54b40a81a5d25f9af830ee9c1ddaca7395
Bayramzade066 <bayramzade.313@gmail.com>
2022-02-04 23:59:00 +0400

    netflix

--- Javascript/react/reacttask5-netflix/src/components/Home.jsx
+++ Javascript/react/reacttask5-netflix/src/components/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../App.css';
 import Navbar from './Navbar';
+import Carousel from './Carousel';
 
 function Home() {
   return <div className='container'>
     <Navbar/>
+    <Carousel/>
   </div>;
 }
 
