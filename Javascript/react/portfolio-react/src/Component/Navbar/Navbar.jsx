342fa35404b2e92912e01d00f9875e69a963cbb8
Bayramzade066 <bayramzade.313@gmail.com>
2022-02-13 15:45:19 +0400

    update

---
+++ Javascript/react/portfolio-react/src/Component/Navbar/Navbar.jsx
@@ -0,0 +1,62 @@
+import React,{useEffect, useState} from 'react'
+import './Navbar.css'
+import { NavLink, useLocation} from 'react-router-dom';
+
+import Logo from './logo.svg';
+
+
+
+function Navbar() {
+
+  const Headerlocation = useLocation()
+
+  const [headeranima, setHeaderanima] = useState(false)
+
+  const  anima = ()=>{
+    setHeaderanima(true)
+    setTimeout(() => {
+      setHeaderanima(false)
+    }, 700);
+  }
+
+  useEffect(() => {
+    anima()
+    
+
+  }, [Headerlocation.pathname])
+  
+
+  return (
+    <header className={headeranima ? "headerActive" : ""}>
+      <div className="navbar">
+        <div className="logo">
+          <img src={Logo} alt="Logo" />
+        </div>
+        <div className="navlinks">
+          <NavLink style={({ isActive }) => ({
+                            color: isActive ? "#444" : '',
+
+                        })} to="/">HOME</NavLink>
+          <NavLink style={({ isActive }) => ({
+                            color: isActive ? "#444" : '',
+
+                        })} to="Work">WORK</NavLink>
+          <NavLink style={({ isActive }) => ({
+                            color: isActive ? "#444" : '',
+
+                        })} to="About">ABOUT</NavLink>
+          <NavLink style={({ isActive }) => ({
+                            color: isActive ? "#444" : '',
+
+                        })} to="Blog">BLOG</NavLink>
+          <NavLink style={({ isActive }) => ({
+                            color: isActive ? "#444" : '',
+
+                        })} to="Contact">CONTACT</NavLink>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Navbar
