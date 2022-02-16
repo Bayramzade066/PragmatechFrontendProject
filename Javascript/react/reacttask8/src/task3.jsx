8ac90348a88d444021037abd108644e04315fc51
Bayramzade066 <bayramzade.313@gmail.com>
2022-02-16 19:03:07 +0400

    task3

--- Javascript/react/reacttask8/src/task3.jsx
+++ Javascript/react/reacttask8/src/task3.jsx
@@ -10,28 +10,50 @@ function App() {
 
 
 
-    const inputval = inputvalue.current.value
 
-    fetch("https://covid-api.mmediagroup.fr/v1/cases")
-        .then(response => response.json())
-        .then(data => {
-            setinfo(data[inputval].All)
 
-        });
-    fetch(" https://covid-api.mmediagroup.fr/v1/vaccines")
-        .then(response => response.json())
-        .then(data => {
-            setvaccine(data[inputval].All)
+    const inputvalue = useRef(null)
 
-        });
+    const show = () => {
+        const inputval = inputvalue.current.value
 
+        fetch("https://covid-api.mmediagroup.fr/v1/cases")
+            .then(response => response.json())
+            .then(data => {
+                setinfo(data[inputval].All)
+
+            });
+        fetch(" https://covid-api.mmediagroup.fr/v1/vaccines")
+            .then(response => response.json())
+            .then(data => {
+                setvaccine(data[inputval].All)
+
+            });
+
+           
+
+
+
+    }
+    useEffect(() => {
+        if (info && vaccine) {
+            setpercentage(Math.round(vaccine.people_vaccinated / info.population  * 100) + "%")
+        }
+
+    }, [info, vaccine])
 
 
 
   return (
-    <div className="task3">
-
+    <div className='task3'>
+    <div className="info">
+        <div className="info__item">Ölüm sayı: {info.deaths}</div>
+        <div className="info__item">Vaksinasiya faizi: {percentage}</div>
+        <div className="info__item">Vaksinasiya sayı: {vaccine.people_vaccinated}</div>
     </div>
+    <input ref={inputvalue} type="text" />
+    <button onClick={show} >Göstər</button>
+</div>
   )
 }
 
