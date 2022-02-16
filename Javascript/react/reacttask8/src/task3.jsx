5170181422bb710f45157b301be8c3e33a29e4fa
Bayramzade066 <bayramzade.313@gmail.com>
2022-02-16 19:01:27 +0400

    task3

--- Javascript/react/reacttask8/src/task3.jsx
+++ Javascript/react/reacttask8/src/task3.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
+import "./task3.css";
 
 function App() {
+
+    const [info, setinfo] = useState("")
+    const [vaccine, setvaccine] = useState("")
+
+    const [percentage, setpercentage] = useState("")
+
+
+
+    const inputval = inputvalue.current.value
+
+    fetch("https://covid-api.mmediagroup.fr/v1/cases")
+        .then(response => response.json())
+        .then(data => {
+            setinfo(data[inputval].All)
+
+        });
+    fetch(" https://covid-api.mmediagroup.fr/v1/vaccines")
+        .then(response => response.json())
+        .then(data => {
+            setvaccine(data[inputval].All)
+
+        });
+
+
+
+
   return (
     <div className="task3">
-        
+
     </div>
   )
 }
